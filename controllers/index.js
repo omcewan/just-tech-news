e0d1fc0553226e6108870f84a60aeb863c4cec7a
omcewan <orlandomcewan231@gmail.com>
2022-03-13 11:44:41 -0400

    install handlebars.js and set up the MVC structure to the code base

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-
-
+const homeRoutes = require('./home-routes.js');
 const apiRoutes = require('./api/index');
 
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
